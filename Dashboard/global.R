8234c554ae1f0af14c2659662c3322d7205c9517
amyrz2 <95899159+amyrz2@users.noreply.github.com>
2023-04-06 17:59:22 -0600

    Delete Dashboard directory

--- Dashboard/global.R
+++
@@ -1,9 +0,0 @@
-# global.R
-
-# Load libraries
-library(shiny)
-library(shinydashboard)
-library(dplyr)
-
-# Define global variables
-my_variable <- "Hello world!"
--- Dashboard/server.R
+++
@@ -1,9 +0,0 @@
-# server.R
-
-# Load global settings
-source("global.R")
-
-# Define server
-server <- function(input, output, session) {
-  # Server logic goes here
-}
